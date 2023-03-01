52cf9ee54093ee137f5dcce9a21beb280a4498f4
mohirwanh <mohirwanh@gmail.com>
2023-03-01 23:12:54 +0700

    Introduction Days

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/message')
+  getMessage(): JSON {
+    const json =
+      '{"browsers":{"firefox":{"name":"Firefox","pref_url":"about:config","releases":{"1":{"release_date":"2004-11-09","status":"retired","engine":"Gecko","engine_version":"1.7"}}}}}';
+    return JSON.parse(json);
+  }
 }
